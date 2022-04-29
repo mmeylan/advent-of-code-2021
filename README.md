db3fa25c97f0872c0688d465acaea7d2736f24f6
Mathieu Meylan <mathieu.meylan@buildigo.ch>
2022-04-29 02:28:50 +0200

    chore: day 1 challenge

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
+# Advent of code 2021 in Rust
+
 https://adventofcode.com/2021
+
+## How to run a challenge
+
+```bash
+# runs day 1
+cargo run 1
+```
+
+## How to run a challenge tests
+
+```bash
+# runs day 1 tests
+cargo test day1_tests
+```
--- src/challenges/day1.rs
+++ src/challenges/day1.rs
@@ -1,3 +1,58 @@
+use std::fs;
+const LINE_ENDING: &'static str = "\n";
+
 pub fn run() {
-    println!("TODO: Day 1")
+    let input = read_input("src/challenges/input.txt");
+    let res = count_depth_measurement_increases(input);
+    println!("Day 1 result, phase 1 = {}", res);
+
+    let input = read_input("src/challenges/input-phase2.txt");
+    let res = count_depth_measurement_increases_phase_2(input);
+    println!("Day 1 result, phase 1 = {}", res);
+}
+
+fn read_input(filename: &str) -> Vec<i32> {
+    match fs::read_to_string(filename) {
+        Ok(content) => {
+            content.trim().split(LINE_ENDING)
+                .filter_map(|s| s.parse::<i32>().ok())
+                .collect()
+        },
+        Err(err) => panic!("{}", err.to_string())
+    }
+}
+
+fn count_depth_measurement_increases(input: Vec<i32>) -> i32 {
+    input.windows(2).map(|window| {
+        if window[1] > window[0] { 1 }
+        else { 0 }
+    }).sum()
+}
+
+fn count_depth_measurement_increases_phase_2(input: Vec<i32>) -> i32 {
+    input.windows(4).map(|window| {
+        let first: i32 = window[0..3].iter().sum();
+        let second: i32 = window[1..4].iter().sum();
+        if second > first { 1 } else { 0 }
+    }).sum()
+}
+
+#[cfg(test)]
+mod day1_tests {
+    use crate::challenges::day1::count_depth_measurement_increases;
+    use crate::challenges::day1::count_depth_measurement_increases_phase_2;
+
+    #[test]
+    fn part1_works_with_example() {
+        let input = vec!(199, 200, 208, 210, 200, 207, 240, 269, 260, 263);
+        let res = count_depth_measurement_increases(input);
+        assert_eq!(res, 7);
+    }
+
+    #[test]
+    fn part2_works_with_example() {
+        let input = vec!(199,200,208,210,200,207,240,269,260,263);
+        let res = count_depth_measurement_increases_phase_2(input);
+        assert_eq!(res, 5)
+    }
 }
